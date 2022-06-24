b310610c393dd2d5c39899ba5fb7b70f63ff8c24
Tina <box2298@gmail.com>
2022-06-24 11:11:11 +0800

    uodated

--- src/app/count-board/count-board.component.ts
+++ src/app/count-board/count-board.component.ts
@@ -17,51 +17,64 @@ export class CountBoardComponent implements OnInit {
   show: boolean = false; //眼睛開關判斷
   noShow: boolean = true; //眼睛開關判斷
   count:string = "$24,055"; //帳戶總資產呈現
+  prevAccountSelected:number=1;
   counts:CountInstance = { //帳戶總資產資料
-    臺幣: {
-      '臺幣帳戶': "$24,055",
-      'count1': "$12,500",
-      'count2': "$11,555"
-    },
+    臺幣: ['$24,055','$12,500','$11,555'], //[臺幣帳總資產,count1資產,count2資產]
     外幣: '$5,055',
     數位: '$79,055',
   };
 
+
   //帳戶切換method
   changeCount(count:any,$event:any) {
+    type ObjectKey = keyof typeof this.counts;
+    let accountKey = count as ObjectKey;
     this.countCategory = count;
-    // 無法判斷count????
-    // this.count = this.counts[count];
-    switch(count){
-      case '臺幣':
-        const countSelected = this.select.nativeElement.value;
-        switch(countSelected){
-          case '臺幣帳戶':
-            this.count = this.counts.臺幣.臺幣帳戶;
-            break;
-            case '1234-5678-9101-1213':
-            this.count = this.counts.臺幣.count1;
-            this.cardShow.card1 = true;
-            this.cardShow.card2 = false;
-            break;
-            case '1234-5678-9101-1215':
-            this.count = this.counts.臺幣.count2;
-            this.cardShow.card1 = false;
-            this.cardShow.card2 = true;
-            break;
-        }
-        break;
-      case '外幣':
-        this.count = this.counts.外幣;
-        this.select.nativeElement.value = '臺幣帳戶';
-        break;
-      case '數位':
-        this.count = this.counts.數位;
-        this.select.nativeElement.value = '臺幣帳戶';
-        break;
-      default:
-        break;
+
+    if(count === '臺幣'){
+      const countSelected = this.select.nativeElement.value;
+      this.count = this.counts.臺幣[countSelected];
+      if(countSelected == 1) {
+        this.cardShow.card1 = true;
+        this.cardShow.card2 = false;
+      }else if(countSelected == 2){
+        this.cardShow.card1 = false;
+        this.cardShow.card2 = true;
+      }
+    }else {
+      this.count = this.counts[accountKey];
     }
+
+    // switch(count){
+    //   case '臺幣':
+    //     const countSelected = this.select.nativeElement.value;
+    //     switch(countSelected){
+    //       case '臺幣帳戶':
+    //         this.count = this.counts.臺幣.臺幣帳戶;
+    //         break;
+    //         case '1234-5678-9101-1213':
+    //         this.count = this.counts.臺幣.count1;
+    //         this.cardShow.card1 = true;
+    //         this.cardShow.card2 = false;
+    //         break;
+    //         case '1234-5678-9101-1215':
+    //         this.count = this.counts.臺幣.count2;
+    //         this.cardShow.card1 = false;
+    //         this.cardShow.card2 = true;
+    //         break;
+    //     }
+    //     break;
+    //   case '外幣':
+    //     this.count = this.counts.外幣;
+    //     this.select.nativeElement.value = '臺幣帳戶';
+    //     break;
+    //   case '數位':
+    //     this.count = this.counts.數位;
+    //     this.select.nativeElement.value = '臺幣帳戶';
+    //     break;
+    //   default:
+    //     break;
+    // }
   }
 
   constructor() { }
